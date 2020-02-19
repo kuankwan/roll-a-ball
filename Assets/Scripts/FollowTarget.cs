73503aa14f2b52038cc97acf8aa40918a6b24636
kuankwan <kuankwan21@163.com>
2020-02-19 16:10:13 +0800

    roll a ball game

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTarget : MonoBehaviour {
+
+    public Transform PlayerTransform;
+
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+
+        offset = transform.position + PlayerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = PlayerTransform.position + offset;
+	
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody rd;
+    public int force = 5;
+    private int score = 0;
+    public Text text;
+    public GameObject wintext;
+
+
+    
+	// Use this for initialization
+	void Start () {
+        rd = GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rd.AddForce(new Vector3(h, 0, v) * force);
+
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //string name = collision.collider.name;
+        if (collision.collider.tag == "PickUps")
+        {
+            Destroy(collision.collider.gameObject);
+        }
+    }
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.tag == "PickUps") {
+            score++;
+            text.text = score.ToString();
+            if (score == 10)
+            {
+                wintext.SetActive(true);
+            }
+			Destroy(collider.gameObject);
+		}
+	}
+}
